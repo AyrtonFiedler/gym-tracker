a6574c8b35b9b8f067a110e19842c8bd2815f35f
AyrtonFiedler <ayrton.fiedler@outlook.com>
2026-09-11 08:18:45 +1000

    Cache V17 notebook interface offline

--- sw.js
+++ sw.js
@@ -1,16 +1,44 @@
-const CACHE = 'gym-tracker-v16';
-const ASSETS = ['./','./index.html','./legacy.html','./layout-v16.css','./layout-v16.js','./apple-touch-icon.png'];
+const CACHE = 'gym-tracker-v17';
+const ASSETS = [
+  './',
+  './index.html',
+  './legacy.html',
+  './layout-v17-css-0.txt',
+  './layout-v17-css-1.txt',
+  './layout-v17-css-2.txt',
+  './layout-v17-js-0.txt',
+  './layout-v17-js-1.txt',
+  './layout-v17-js-2.txt',
+  './layout-v17-js-3.txt',
+  './layout-v17-js-4.txt',
+  './layout-v17-js-5.txt',
+  './apple-touch-icon.png'
+];
+
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
+
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
+
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request, {ignoreSearch:true}).then(hit => hit || fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
-    return response;
-  }).catch(() => caches.match('./index.html'))));
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true})
+      .then(hit => hit || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      }).catch(() => caches.match('./index.html')))
+  );
 });
